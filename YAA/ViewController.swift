dab51084e0a63f932f53ff4c528857fb6e9d8b49
Bojan <bojanmitreski@outlook.com>
2023-05-01 22:18:48 +0200

    image0 added to button

--- YAA/ViewController.swift
+++ YAA/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +19,8 @@ class ViewController: UIViewController {
 
     @IBAction func messageButton(_ sender: UIButton) {
         messageLabel.text = "You Are Awesome!"
+        messageLabel.textAlignment = .center
+        imageView.image = UIImage(named: "image0")
     }
     
 }
