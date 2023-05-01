b202513ba4548dfe8db9d395793987e85cfbc709
Bojan <bojanmitreski@outlook.com>
2023-05-02 01:39:30 +0200

    CH. 2.9 -> Int to String conversion

--- YAA/ViewController.swift
+++ YAA/ViewController.swift
@@ -12,15 +12,30 @@ class ViewController: UIViewController {
     @IBOutlet weak var messageLabel: UILabel!
     @IBOutlet weak var imageView: UIImageView!
     
+    var imageNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        messageLabel.textAlignment = .center
         // Do any additional setup after loading the view.
     }
-
+    
     @IBAction func messageButton(_ sender: UIButton) {
-        messageLabel.text = "You Are Awesome!"
-        messageLabel.textAlignment = .center
-        imageView.image = UIImage(named: "image0")
+        
+        imageView.image = UIImage(named: "image" + String(imageNumber))
+        imageNumber += 1
+        if imageNumber == 10 {
+            imageNumber = 0
+        }
+        
+        
+//        if messageLabel.text == "You Are Awesome!" {
+//            messageLabel.text = "You Are Great!"
+//            imageView.image = UIImage(named: "image1")
+//        } else {
+//            messageLabel.text = "You Are Awesome!"
+//            imageView.image = UIImage(named: "image0")
+//        }
     }
     
 }
